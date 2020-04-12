b50780b96a528283dd86a5e300007aa4e7a89855
tcarroll2 <tc98207@gmail.com>
2020-04-12 11:44:34 -0500

    Page 147

--- Project1/SourceViewController.swift
+++ Project1/SourceViewController.swift
@@ -2,7 +2,7 @@
 //  SourceViewController.swift
 //  Project1
 //
-//  Created by Thomas Carroll on 4/12/20.
+//  Created by Thomas Carroll on 4/12/20..
 //  Copyright © 2020 Thomas Carroll. All rights reserved.
 //
 
