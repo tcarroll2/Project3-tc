d3894edd563515c4b8c92ab1e638533cba4fa0c5
tcarroll2 <tc98207@gmail.com>
2020-04-12 10:50:41 -0500

    This is the initial commit.

---
+++ Project1/SourceViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SourceViewController.swift
+//  Project1
+//
+//  Created by Thomas Carroll on 4/12/20.
+//  Copyright © 2020 Thomas Carroll. All rights reserved.
+//
+
+import Cocoa
+
+class SourceViewController: NSViewController {
+
+    @IBOutlet var tableView: NSTableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
