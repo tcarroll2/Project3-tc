9052c0abfdb2b7005b6113e39c8276b4021dc0e1
tcarroll2 <tc98207@gmail.com>
2020-04-12 11:31:27 -0500

    Page 147

--- Project1/SourceViewController.swift
+++ Project1/SourceViewController.swift
@@ -21,4 +21,10 @@ class SourceViewController: NSViewController, NSTableViewDataSource, NSTableView
         return 100
     }
     
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        guard let vw = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView else
+            { return nil }
+        vw.textField?.stringValue = "Hello, world!"
+        return vw
+    }
 }
